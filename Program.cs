2fa60695b55ed923c17659dca9a05a23c9486ad5
nicolasnunosh <nicomunosh@gmail.com>
2026-03-10 11:38:58 -0500

    feat: add menu input reading

--- Program.cs
+++ Program.cs
@@ -8,15 +8,20 @@ class Program
     }
 
     static void ShowMainMenu()
-    {
-        Console.WriteLine("===== SISTEMA BIBLIOTECA =====");
-        Console.WriteLine("1. Libros");
-        Console.WriteLine("2. Usuarios");
-        Console.WriteLine("3. Préstamos");
-        Console.WriteLine("4. Búsquedas y reportes");
-        Console.WriteLine("5. Guardar / Cargar datos");
-        Console.WriteLine("6. Salir");
-    }
+{
+    Console.WriteLine("===== SISTEMA BIBLIOTECA =====");
+    Console.WriteLine("1. Libros");
+    Console.WriteLine("2. Usuarios");
+    Console.WriteLine("3. Préstamos");
+    Console.WriteLine("4. Búsquedas y reportes");
+    Console.WriteLine("5. Guardar / Cargar datos");
+    Console.WriteLine("6. Salir");
+
+    Console.Write("Seleccione una opción: ");
+
+    int option;
+    int.TryParse(Console.ReadLine(), out option);
+}
 
     static void ShowBooksMenu() {}
     static void ShowUsersMenu() {}
